568145d591781ebf9c3396df670015f484e65032
Guillaume <guillaume.vincent1138@gmail.com>
2024-11-28 12:11:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 
 const fizzbuzz = (nb) => {
     if (nb % 3 == 0 && nb % 5 == 0) {
-      return console.log(nb, "fizz buzz")
+      return console.log(nb, "FizzBuzz")
     }
     if (nb % 3 == 0) {
-      return console.log(nb, "fizz")
+      return console.log(nb, "Fizz")
     }
     if (nb % 5 == 0) {
-      return console.log(nb, "buzz")
+      return console.log(nb, "Buzz")
     } else return console.log(nb)
   }
   
