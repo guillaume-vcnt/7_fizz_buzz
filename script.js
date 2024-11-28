d305659ec78b20f625804ace272ec48865a2ac7f
Guillaume <anonyme@example.com>
2024-11-28 12:05:54 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// FIZZ BUZZ
+
+// 1 = number, 3 = fizz, 5 = buzz, 15 = fizzbuzz
+
+const fizzbuzz = (nb) => {
+    if (nb % 3 == 0 && nb % 5 == 0) {
+      return console.log(nb, "fizz buzz")
+    }
+    if (nb % 3 == 0) {
+      return console.log(nb, "fizz")
+    }
+    if (nb % 5 == 0) {
+      return console.log(nb, "buzz")
+    } else return console.log(nb)
+  }
+  
+  fizzbuzz(1)
+  fizzbuzz(3)
+  fizzbuzz(5)
+  fizzbuzz(15)
+  
